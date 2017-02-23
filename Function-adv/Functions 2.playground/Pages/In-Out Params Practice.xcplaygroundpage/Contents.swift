1e149938599131f79bf45872048418405d628b3c
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-02-23 12:43:34 +0530

    feat: complete functions-adv

--- Function-adv/Functions 2.playground/Pages/In-Out Params Practice.xcplaygroundpage/Contents.swift
+++ Function-adv/Functions 2.playground/Pages/In-Out Params Practice.xcplaygroundpage/Contents.swift
@@ -8,6 +8,17 @@ struct EmployeeData {
 //: Define a function called `increasePay` that takes 1 `EmployeeData` parameter called `employeeData` and 1 `Double` parameter called `percentRaise`. **`employeeData` should be a inout parameter**. The function should **add** to its `baseSalary` property by a value of `baseSalary * percentRaise` and increase the `raisesGiven` property by 1.
 //: **The solution is available on the next page!**
 
+func increasePay(employeeData: inout EmployeeData, percentRaise: Double){
+    employeeData.baseSalary += (employeeData.baseSalary * percentRaise)
+    employeeData.raisesGiven += 1
+}
 
+var employee = EmployeeData(baseSalary: 15000, raisesGiven: 0)
+
+employee.baseSalary
+employee.raisesGiven
+increasePay(employeeData: &employee, percentRaise: 0.10)
+employee.baseSalary
+employee.raisesGiven
 
 //: [Next](@next)
